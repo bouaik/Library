b53028070c4dbe0254d8eb66bf8c76331371bf2e
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-07 15:37:16 +0100

    fix eslint errors

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
-(function () {
+(function library() {
+  const submit = document.querySelector('.submitBtn');
+  const bookList = document.querySelector('.book-list');
 
-    let myLibrary = [];
 
-    const submit = document.querySelector('.submitBtn')
-    const bookList = document.querySelector('.book-list')
+  function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-    submit.addEventListener('click', addBookToLibrary)
-    bookList.addEventListener('click', removeItem)
-    document.addEventListener('DOMContentLoaded', displaystorage)
+  function clearfields() {
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
+    document.querySelector('.pages').value = '';
+    const read = document.getElementsByName('customRadioInline1');
+    read.forEach((ele) => {
+      ele.checked = false;
+    });
+  }
 
-    function Book(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-
-    function addBookToLibrary(e) {
-        e.preventDefault() 
-        let res
-        const title = document.querySelector('.title').value
-        const author = document.querySelector('.author').value
-        const pages = document.querySelector('.pages').value
-        const read = document.getElementsByName('customRadioInline1'); 
-        read.forEach( (ele) => {
-            if(ele.checked) {
-                res = ele.value
-            }  
-        })
-
-        const book = new Book(title, author, pages, res)
-        display(book)
-        updateStorage(book)
-    }
-
-    function display(book) {
-        const bookDisplay = document.createElement('div')
-        bookDisplay.classList.add('col-11', 'mx-auto', 'col-md-6', 'col-lg-4', 'my-3')
-        bookDisplay.innerHTML = `<div class="card text-left">
+  function display(book) {
+    const bookDisplay = document.createElement('div');
+    bookDisplay.classList.add('col-11', 'mx-auto', 'col-md-6', 'col-lg-4', 'my-3');
+    bookDisplay.innerHTML = `<div class="card text-left">
                             <div class="card-body">
                                 <h6 class="text-capitalize "><span class="badge badge-warning mr-2">title :</span><span
                                         id="book-title">${book.title}</span></h6>
@@ -51,53 +37,65 @@
                                     :</span><span id="book-red">${book.read}</span></h6>
                                 <a class="text-center delete-link d-block"><i class="far fa-trash-alt"></i></a>
                             </div>
-                        </div>`
+                        </div>`;
 
-        bookList.appendChild(bookDisplay)
-        clearfields() 
+    bookList.appendChild(bookDisplay);
+    clearfields();
+  }
+
+  function displaystorage() {
+    const exists = localStorage.getItem('bookList');
+
+    if (exists) {
+      const storageItems = JSON.parse(localStorage.getItem('bookList'));
+
+      storageItems.forEach((element) => {
+        display(element);
+      });
     }
-
-    function removeItem (e) {
-        e.preventDefault()
-        let link = e.target.parentElement
-        if (link.classList.contains('delete-link')) {
-            let book = link.parentElement.parentElement.parentElement
-            bookList.removeChild(book)
-        }
-    }
+  }
 
 
-    function clearfields() {
-        document.querySelector('.title').value = ""
-        document.querySelector('.author').value = ""
-        document.querySelector('.pages').value = ""
-        const read = document.getElementsByName('customRadioInline1'); 
-        read.forEach( (ele) => {
-            ele.checked = false
-        })
+  document.addEventListener('DOMContentLoaded', displaystorage);
+
+
+  function updateStorage(book) {
+    const bookList = localStorage.getItem('bookList') ? JSON.parse(localStorage.getItem('bookList')) : [];
+    bookList.push(book);
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+  }
+
+  function addBookToLibrary(e) {
+    e.preventDefault();
+    let res;
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
+    const pages = document.querySelector('.pages').value;
+    const read = document.getElementsByName('customRadioInline1');
+    read.forEach((ele) => {
+      if (ele.checked) {
+        res = ele.value;
       }
+    });
 
 
-    function updateStorage(book) {
-        let bookList 
-        
-        bookList = localStorage.getItem('bookList') ? JSON.parse(localStorage.getItem('bookList')) : []
-        bookList.push(book)
-        localStorage.setItem('bookList', JSON.stringify(bookList))
+    const book = new Book(title, author, pages, res);
+    display(book);
+    updateStorage(book);
+  }
+
+
+  submit.addEventListener('click', addBookToLibrary);
+
+  function removeItem(e) {
+    e.preventDefault();
+    const link = e.target.parentElement;
+    if (link.classList.contains('delete-link')) {
+      const book = link.parentElement.parentElement.parentElement;
+      bookList.removeChild(book);
     }
-
-    function displaystorage () {
-        let exists = localStorage.getItem('bookList')
-    
-        if(exists) {
-            let storageItems = JSON.parse(localStorage.getItem('bookList'))
-    
-            storageItems.forEach( function (element) {
-                display(element)
-            })
-        }
-    }
+  }
 
 
-
-})();
+  bookList.addEventListener('click', removeItem);
+}());
