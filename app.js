a41fbc90a95882bca798ae6f3ba44b140392f7d6
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-07 12:47:04 +0100

    display book

--- app.js
+++ app.js
@@ -27,11 +27,34 @@
         })
 
         const book = new Book(title, author, pages, res)
-        myLibrary.push(book)
-
-        
+        display(book)
     }
 
+
+    myLibrary.forEach( book => display(book))
+
+    function display(book) {
+        const bookList = document.querySelector('.book-list')
+        const bookDisplay = document.createElement('div')
+        bookDisplay.classList.add('col-11', 'mx-auto', 'col-md-6', 'col-lg-4', 'my-3')
+        bookDisplay.innerHTML = `<div class="card text-left">
+                            <div class="card-body">
+                                <h6 class="text-capitalize "><span class="badge badge-warning mr-2">title :</span><span
+                                        id="book-title">${book.title}</span></h6>
+                                <h6 class="text-capitalize my-3"><span class="badge badge-success mr-2">author
+                                        :</span><span id="book-author">
+                                        ${book.author}
+                                    </span></h6>
+                                <h6 class="text-capitalize"><span class="badge badge-danger mr-2">number of pages
+                                        :</span><span id="book-pages">${book.pages}</span></h6>
+                                <h6 class="text-capitalize"><span class="badge badge-info mr-2">Read ?
+                                    :</span><span id="book-red">${book.read}</span></h6>
+                            </div>
+                        </div>`
+
+        bookList.appendChild(bookDisplay)
+    }
+    
 
 
 })();
